a866c66b3269b54c78a77f7b999578638b14d4e0
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-11 20:03:50 +0000

    adding step 2 functionality

--- printing-buffered.js
+++ printing-buffered.js
@@ -1,42 +1,24 @@
 var https = require('https');
 
 function getAndPrintHTML () {
-
-  var requestOptions = {
+ var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
 
-  /* Add your code here */
+  https.get(requestOptions, function(response){
+    response.setEncoding('utf8');
 
+    var rawData = '';
+    response.on('data', function (data) {
+      rawData += data;
+    })
 
-// notice that https.get takes a callback with one parameter -
-// response, which is a Stream that represents the HTTP response
-  https.get(requestOptions, function (response) {
+    response.on('end', function() {
+    console.log(rawData);
+    });
 
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
-
-  let rawData = '';
-  // the callback is invoked when a `data` chunk is received
-  response.on('data', function (data) {
-    console.log('Chunk Received. Length:', data.length);
-    rawData += data;
-  });
-
-  // the callback is invoked when all of the data has been received
-  // (the `end` of the stream)
-  response.on('end', function() {
-    console.log('Response stream complete.');
-
-    var parsedData = JSON.parse(rawData);
-
-    for (let i = 0; i < parsedData.length; i++)
-      console.log(parsedData[i]);
-  });
-
-});
-
+  })
 }
 
 getAndPrintHTML();
